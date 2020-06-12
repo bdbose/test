28e6bdfe765b52b16e83b86f54ebeda3b5f7dce4
bdbose <57151177+bdbose@users.noreply.github.com>
2020-06-12 15:49:22 +0530

    Updates

---
+++ precache-manifest.2264ec3b1a135284d3b6d452fb843871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5c0fd9220fd98035eb1",
+    "url": "/test/static/css/main.6e6cb67a.chunk.css"
+  },
+  {
+    "revision": "a5c0fd9220fd98035eb1",
+    "url": "/test/static/js/main.27e7e2dc.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "d621a1c018c37a9a4ab6",
+    "url": "/test/static/js/2.647fa92d.chunk.js"
+  },
+  {
+    "revision": "165fab99a2f728055ace62cf1fbaa6f5",
+    "url": "/test/index.html"
+  }
+];
